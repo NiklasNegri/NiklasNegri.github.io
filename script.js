53a343695fc048fc58bb4dc87f18c8db5d29ca6c
NiklasNegri <69984406+NiklasNegri@users.noreply.github.com>
2022-03-06 22:19:23 +0100

    hosting todomvc

--- script.js
+++ script.js
@@ -1,71 +1,27 @@
 const todoList = document.querySelector(".todo-list");
-const newTodoInput = document.querySelector(".new-todo-input");
-const newTodoForm = document.querySelector(".new-todo-form");
+const addTodoInpur = document.querySelector(".add-todo-input");
+const newTodoForm = document.querySelector(".new-todo");
 const clearAllButton = document.querySelector(".clear-all-button");
 const itemsLeft = document.querySelector(".items-left");
-const itemsShowAllButton = document.querySelector(".items-show-all");
-const itemsActiveButton = document.querySelector(".items-active");
-const itemsCompletedButton = document.querySelector(".items-completed");
-const toggleAll = document.querySelector("#toggle-all");
+const showAllButton = document.querySelector(".all-button");
+const activeButton = document.querySelector(".active-button");
+const completedButton = document.querySelector(".completed-button");
+const toggleAll = document.querySelector("#arrow-button");
 const toggleLabel = document.querySelector(".toggle-label");
-const filters = document.querySelector(".filters");
-
+const bottomBarList = document.querySelector(".bottom-bar-list");
+const replacementBar = document.querySelector(".replacement-bar")
 let completedItems = 0;
 let uncompletedItems = 0;
-filters.style.display = 'none';
+let filter = setFilter("all");
+bottomBarList.style.display = 'none';
 
 newTodoForm.onsubmit = event => {
     event.preventDefault();
-    if (newTodoInput.value) {
-        addTodo(newTodoInput.value);
-        setFilter("all");
-        toggleAll.checked = false;
-        newTodoInput.value = '';
+    if (addTodoInpur.value) {
+        addTodo(addTodoInpur.value);
+        addTodoInpur.value = '';
+        setFilter(filter);
     }
-}
-
-clearAllButton.onclick = event => {
-    clearAllButton.hidden = true;
-    for (let c of document.querySelector(".todo-list").querySelectorAll('input[type="checkbox"]')) {
-        if (c.checked) {
-            c.parentNode.parentNode.removeChild(c.parentNode);
-            completedItems--;
-        }
-    }
-    displayItemsLeft();
-}
-
-toggleAll.onclick = event => {
-    if (toggleAll.checked) {
-        for (let c of document.querySelector(".todo-list").querySelectorAll('input[type="checkbox"]')) {
-            if (!c.checked) {
-                c.checked = true;
-                uncompletedItems--;
-                completedItems++
-                c.parentElement.setAttribute("class", "completed");
-            }
-        }
-    }
-    else {
-        for (let c of document.querySelector(".todo-list").querySelectorAll('input[type="checkbox"]')) {
-            c.checked = false;
-            uncompletedItems++;
-            completedItems--;
-            c.parentElement.removeAttribute("class");
-        }
-    }
-    displayItemsLeft();
-    displayClearAllButton();
-}
-
-itemsActiveButton.onclick = event => {
-    setFilter("active");
-}
-itemsCompletedButton.onclick = event => {
-    setFilter("completed");
-}
-itemsShowAllButton.onclick = event => {
-    setFilter("all");
 }
 function addTodo(todoText) {
     let liCheckbox = document.createElement('input');
@@ -84,7 +40,7 @@ function addTodo(todoText) {
 
     uncompletedItems++;
     displayItemsLeft();
-    filters.style.display = 'flex';
+    bottomBarList.style.display = 'flex';
 
     liRemoveButton.onclick = event => {
         if (!liCheckbox.checked) {
@@ -111,44 +67,67 @@ function addTodo(todoText) {
         displayItemsLeft();
         displayClearAllButton();
     }
+    replacementBar.hidden = true;
 }
-function displayClearAllButton() {
-    if (completedItems > 0) {
-        clearAllButton.hidden = false;
-    }
-    else if (completedItems === 0) {
-        clearAllButton.hidden = true;
-    }
-}
-function displayItemsLeft() {
-    if (uncompletedItems === 0 && completedItems === 0) {
-        toggleAll.hidden = true;
-        toggleAll.checked = false;
-        toggleLabel.hidden = true;
-        filters.style.display = 'none';
-        itemsLeft.textContent = '0 items left';
-    }
-    else if (uncompletedItems === 1) {
-        toggleAll.hidden = false;
-        toggleLabel.hidden = false;
-        itemsLeft.textContent = uncompletedItems + ' item left';
+toggleAll.onclick = event => {
+    if (isAllToggled()) {
+        for (let c of document.querySelector(".todo-list").querySelectorAll('input[type="checkbox"]')) {
+            c.checked = false;
+            uncompletedItems++;
+            completedItems--;
+            c.parentElement.removeAttribute("class");
+        }
     }
     else {
-        toggleAll.hidden = false;
-        toggleLabel.hidden = false;
-        itemsLeft.textContent = uncompletedItems + ' items left';
+        for (let c of document.querySelector(".todo-list").querySelectorAll('input[type="checkbox"]')) {
+            if (!c.checked) {
+                c.checked = true;
+                uncompletedItems--;
+                completedItems++
+                c.parentElement.setAttribute("class", "completed");
+            }
+        }
     }
+
+    setFilter(filter)
+    displayItemsLeft();
+    displayClearAllButton();
+}
+function isAllToggled() {
+    let amountToggled = 0;
+    let idAmount = 0;
+    for (let c of document.querySelector(".todo-list").querySelectorAll('input[type="checkbox"]')) {
+        idAmount++;
+        if (c.checked) {
+            amountToggled++;
+        }
+    }
+    if (amountToggled === idAmount) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+activeButton.onclick = event => {
+    filter = setFilter("active");
+}
+completedButton.onclick = event => {
+    filter = setFilter("completed");
+}
+showAllButton.onclick = event => {
+    filter = setFilter("all");
 }
 function setFilter(newFilter) {
     if (newFilter === "all") {
         for (let c of document.querySelector(".todo-list").querySelectorAll('input[type="checkbox"]')) {
-            c.parentNode.style.display = 'flex';
+            c.parentNode.style.display = 'grid';
         }
     }
     else if (newFilter === "active") {
         for (let c of document.querySelector(".todo-list").querySelectorAll('input[type="checkbox"]')) {
             if (!c.checked) {
-                c.parentNode.style.display = 'flex';
+                c.parentNode.style.display = 'grid';
             }
             else {
                 c.parentNode.style.display = 'none';
@@ -161,8 +140,46 @@ function setFilter(newFilter) {
                 c.parentNode.style.display = 'none';
             }
             else {
-                c.parentNode.style.display = 'flex';
+                c.parentNode.style.display = 'grid';
             }
         }
+    }
+    return newFilter;
+}
+clearAllButton.onclick = event => {
+    clearAllButton.hidden = true;
+    for (let c of document.querySelector(".todo-list").querySelectorAll('input[type="checkbox"]')) {
+        if (c.checked) {
+            c.parentNode.parentNode.removeChild(c.parentNode);
+            completedItems--;
+        }
+    }
+    displayItemsLeft();
+    if (completedItems == 0 && uncompletedItems == 0) {
+        replacementBar.hidden = false;
+    }
+}
+function displayClearAllButton() {
+    if (completedItems > 0) {
+        clearAllButton.hidden = false;
+    }
+    else if (completedItems === 0) {
+        clearAllButton.hidden = true;
+    }
+}
+function displayItemsLeft() {
+    if (uncompletedItems === 0 && completedItems === 0) {
+        toggleAll.checked = false;
+        toggleLabel.hidden = true;
+        bottomBarList.style.display = 'none';
+        itemsLeft.textContent = '0 items left';
+    }
+    else if (uncompletedItems === 1) {
+        toggleLabel.hidden = false;
+        itemsLeft.textContent = uncompletedItems + ' item left';
+    }
+    else {
+        toggleLabel.hidden = false;
+        itemsLeft.textContent = uncompletedItems + ' items left';
     }
 }
