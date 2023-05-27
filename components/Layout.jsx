df5effa49360de80756616f1bd1d9f3691a4dc6e
NiklasNegri <niklas.negri@gmail.com>
2023-05-27 19:12:21 +0200

    modify Layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,10 @@
 import Navbar from './Navbar';
-import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
       <main>{children}</main>
-      <Footer />
     </>
   )
 }
