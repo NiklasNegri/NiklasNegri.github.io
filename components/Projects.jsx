8e5a63f12ca11586da3a7652aba462ae01affa53
NiklasNegri <niklas.negri@gmail.com>
2023-05-28 00:45:04 +0200

    Update project and skills components

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,38 +1,40 @@
+import Image from "next/image";
+
 const projectData = [
     {
         id: 1,
-        title: 'Todo List App',
+        title: 'Curriculum Vitae',
         description:
-            'A simple Todo List App built with JavaScript. All datas are stored in localstorage. It helps users check list out their plans and tick as they do them.',
-        gitHubLink: 'https://github.com/olawanlejoel/Todo-List-App',
+            'A simple one-page Next.js application that uses Static Site Generation without data. Hosted on GitHub pages with worksflows.',
+        gitHubLink: 'https://github.com/NiklasNegri/NiklasNegri.github.io',
     },
     {
         id: 2,
-        title: 'Books Library App',
+        title: 'TBA',
         description:
-            'A simple Book Library App built with JavaScript. It helps readers have a good list of books they are either currently reading or have finished reading.',
-        gitHubLink: 'https://github.com/olawanlejoel/Book-Library',
+            '',
+        gitHubLink: '',
     },
     {
         id: 3,
-        title: 'Quotes Generator',
+        title: 'TBA',
         description:
-            'Helps you generate quotes from about 1600 quotes written by different authors . Quotes are automatically copied to your clipboards.',
-        gitHubLink: 'https://github.com/olawanlejoel/random-quote-generator',
+            '',
+        gitHubLink: '',
     },
     {
         id: 4,
-        title: 'Password Generator',
+        title: 'TBA',
         description:
-            'Helps you generates random passwords, you can select what you want your password to entail and also you can copy generated password to clipboard.',
-        gitHubLink: 'https://github.com/olawanlejoel/Password-Generator',
+            '',
+        gitHubLink: '',
     },
     {
         id: 5,
-        title: 'Twitter UI Clone',
+        title: 'TBA',
         description:
-            'Simple Twitter UI clone built with TailwindCSS and Vue Js. This covers only the homepage of Twitter UI. This is cool to get started with TailwindCSS as it helps understand basic concepts.',
-        gitHubLink: 'https://github.com/olawanlejoel/TwitterUI-clone',
+            '',
+        gitHubLink: '',
     },
 ];
 
@@ -45,9 +47,9 @@ const Projects = () => {
                     {projectData && projectData.map((project) => (
                         <div className="project-card" key={project.id}>
                             <div className="project-header">
-                                <i className="fa-regular fa-folder-open folder-icon"></i>
+                                <Image src="/images/icongitrepository.svg" width={35} height={35} />
                                 <div className="small-icons">
-                                    <a href={project.gitHubLink}><i className="fa-brands fa-github"></i></a>
+                                    <a href={project.gitHubLink}><Image src="/images/icongithub.svg" width={35} height={35} /></a>
                                 </div>
                             </div>
                             <h3>{project.title}</h3>
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -6,51 +6,51 @@ const Skills = () => {
             <div className="skills-container">
                 <h2>Skills</h2>
                 <div className="grid-skills">
-                    <div className="skill-card csharp">
+                    <div className="skill-card purple">
                         <Image src="/images/iconcsharp.svg" width={35} height={35} />
                         <p>C#</p>
                     </div>
-                    <div className="skill-card dotnet6">
+                    <div className="skill-card purple">
                         <Image src="/images/icondotnet6.png" width={35} height={35} />
                         <p>.NET 6</p>
                     </div>
-                    <div className="skill-card efcore">
+                    <div className="skill-card purple">
                         <Image src="/images/iconefcore.png" width={35} height={35} />
                         <p>EF Core</p>
                     </div>
-                    <div className="skill-card visualstudio">
+                    <div className="skill-card purple">
                         <Image src="/images/iconvisualstudios.png" width={35} height={35} />
                         <p>Visual Studios</p>
                     </div>
-                    <div className="skill-card sql">
+                    <div className="skill-card blue">
                         <Image src="/images/iconsql.png" width={35} height={35} />
                         <p>SQL / T-SQL</p>
                     </div>
-                    <div className="skill-card atlassian">
+                    <div className="skill-card blue">
                         <Image src="/images/iconatlassian.png" width={35} height={35} />
                         <p>Atlassian Jira/Confluence</p>
                     </div>
-                    <div className="skill-card css">
+                    <div className="skill-card blue">
                         <Image src="/images/iconcss.svg" width={35} height={35} />
                         <p>CSS</p>
                     </div>
-                    <div className="skill-card azure">
+                    <div className="skill-card blue">
                         <Image src="/images/iconazure.svg" width={35} height={35} />
                         <p>Azure</p>
                     </div>
-                    <div className="skill-card ssms">
+                    <div className="skill-card yellow">
                         <Image src="/images/iconssms.png" width={35} height={35} />
                         <p>SSMS</p>
                     </div>
-                    <div className="skill-card js">
+                    <div className="skill-card yellow">
                         <Image src="/images/iconjs.svg" width={35} height={35} />
                         <p>JavaScript</p>
                     </div>
-                    <div className="skill-card nextjs">
+                    <div className="skill-card grey">
                         <Image src="/images/iconnextjs.svg" width={35} height={35} />
                         <p>Next.js</p>
                     </div>
-                    <div className="skill-card html">
+                    <div className="skill-card grey">
                         <Image src="/images/iconhtml5.svg" width={35} height={35} />
                         <p>HTML5</p>
                     </div>
