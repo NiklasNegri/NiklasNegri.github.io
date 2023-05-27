0a548fd4b159ed3745c27744cbdfbf9d86a6f185
NiklasNegri <niklas.negri@gmail.com>
2023-05-27 20:09:24 +0200

    update index.js and styling

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,25 @@
 import Head from 'next/head';
-import Hero from '../components/Hero'
+import About from '/components/About';
+import Experience from '../components/Experience';
+import Skills from '/components/Skills';
+import Projects from '../components/Projects';
+import Contact from '/components/Contact';
 
 const Home = () => {
   return (
     <>
       <Head>
-        <title>Niklas portfolio</title>
-        <meta name="description" content="Niklas Negri portfolio" />
+        <title>Niklas Negri</title>
+        <meta name="description" content="Niklas Negri" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <Hero />
+        <About />
+        <Experience />
+        <Skills />
+        <Projects />
+        <Contact />
       </div>
     </>
   );
