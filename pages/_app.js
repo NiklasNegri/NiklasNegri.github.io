3705b23036960bb93dc721dd4a1bcb104df97f94
NiklasNegri <niklas.negri@gmail.com>
2023-05-27 19:10:58 +0200

    Remove font awesome imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,5 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { config } from "@fortawesome/fontawesome-svg-core";
-
-library.add(faGithub, faLinkedin);
-config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
   return (
