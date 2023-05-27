b750eae251b7e042a72feb68d1f334ffff4c221d
NiklasNegri <niklas.negri@gmail.com>
2023-05-27 20:10:19 +0200

    Add experience and projects components

---
+++ components/Experience.jsx
@@ -0,0 +1,28 @@
+const Experience = () => {
+    return (
+        <div id="experience-section">
+            <div className="experience-container">
+                <h2>Experience</h2>
+                <div className="flex-experience">
+                    <div className="experience-text">
+                        <h3>Internship</h3>
+
+                        <p>October 2022 - December 2022</p>
+                        <p>February 2023 - May 2023</p>
+                    </div>
+                    <div className="experience-text">
+                        <h3>Backend developer, Antura AB</h3>
+                        <p>
+                            Did two internships at Antura AB where I worked with their live production code
+                            as a member of one of their two backend teams. I worked mainly with C# and T-SQL
+                            and both fixed buggs and wrote new features depending on what the product owners
+                            had in store for us.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Experience;
