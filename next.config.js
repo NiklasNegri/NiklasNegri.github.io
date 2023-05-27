12ac3a862419965247d250df345e155e0a81beca
NiklasNegri <niklas.negri@gmail.com>
2023-05-27 02:47:03 +0200

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+  
+   // Add basePath
+    basePath: '/pages',
+  
+  }
+  
+  module.exports = nextConfig
