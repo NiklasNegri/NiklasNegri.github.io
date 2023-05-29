7b3ea2ff07d7fa7294d83c8523a2a8b1f81d5dae
NiklasNegri <niklas.negri@gmail.com>
2023-05-29 15:32:02 +0200

    Fixed responsiveness

--- components/Education.jsx
+++ components/Education.jsx
@@ -10,10 +10,10 @@ const Education = () => {
                         <p>August 2021 - June 2023</p>
                     </div>
                     <div className="experience-text">
-                        <p>•Backend in .NET 6 creating Web Apis with ASP.NET Core and Entity Framework Core</p>
-                        <p>•Basic frontend with HTML, CSS and JavaScript and some work in Vue</p>
-                        <p>•Agile development in Azure with Azure Board and Pipelines for CI/CD</p>
-                        <p>•Hosting an MVC application with ASP.NET Core to Azure Webb App Service</p>
+                        <p>• Backend in .NET 6 creating Web Apis with ASP.NET Core and Entity Framework Core</p>
+                        <p>• Basic frontend with HTML, CSS and JavaScript and some work in Vue</p>
+                        <p>• Agile development in Azure with Azure Board and Pipelines for CI/CD</p>
+                        <p>• Hosting an MVC application with ASP.NET Core to Azure Webb App Service</p>
                     </div>
                 </div>
             </div>
--- components/Experience.jsx
+++ components/Experience.jsx
@@ -11,11 +11,11 @@ const Experience = () => {
                         <p>February 2023 - May 2023</p>
                     </div>
                     <div className="experience-text">
-                        <p>•Worked on live production code from day one for a service that has over 250000 users</p>
-                        <p>•Fixed bugs and wrote new features for Antura Projects</p>
-                        <p>•Mainly C# and T-SQL, but also worked with JS and F#</p>
-                        <p>•Was able to get into the workflow and quickly become a productive member of our team </p>
-                        <p>•Have two references that are available to be contacted</p>
+                        <p>• Worked on live production code from day one for a service that has over 250000 users</p>
+                        <p>• Fixed bugs and wrote new features for Antura Projects</p>
+                        <p>• Mainly C# and T-SQL, but also worked with JS and F#</p>
+                        <p>• Was able to get into the workflow and quickly become a productive member of our team </p>
+                        <p>• Have two references that are available to be contacted</p>
                     </div>
                 </div>
             </div>
